2b2f47fd47be581c59b6a83c574bcf710ac2ddea
Terry Abu <terryabu67@gmail.com>
2022-11-30 07:09:49 -0800

    fourth commit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
-    print("{}".format(chr(letter)),end="")
+    if chr(letter) == 'e' or chr(letter) == 'q':
+        continue
+    print("{}".format(chr(letter)), end="")
