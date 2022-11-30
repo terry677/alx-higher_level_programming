5742e3e28ff24467b37ffb125acf3e3b432795aa
Terry Abu <terryabu67@gmail.com>
2022-11-30 08:47:03 -0800

    13th commit

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzBuzz():
    for number in range(1, 101):
-       if number % 3 and number % 5:
+        if number % 3 and number % 5:
            print("FizzBuzz", end="")
         elif number % 3:
             print("Fizz", end="")
