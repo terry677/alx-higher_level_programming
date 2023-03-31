eec5bfd41fbd41f67a835118440419922511560e
terry677 <terryabu67@gmail.com>
2023-03-30 18:05:58 -0700

    added advance tasks

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# gets content-length of a web page
-curl -I -s $1 | grep HTTP1/1 | cut -d ' ' -f 2
+# sends a request to a URL passed as an argument, and displays only the status code of the response
+curl -so /dev/null --write-out "%{http_code}" "$1"
