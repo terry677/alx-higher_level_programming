1e7723243b94f0636b4575d42a0c36fbc5529663
terry677 <terryabu67@gmail.com>
2023-04-02 05:23:37 -0700

    Added task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,13 +3,11 @@
 A python script that fetches https://alx-intranet.hbtn.io/status
 """
 if __name__ == "__main__":
-
-
     import urllib.request
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
         result = response.read()
         print("Body response:")
         print("\t- type {}".format(type(result)))
         print("\t- content: {}".format(result))
         print("\t- utf8 content: {}".format(result.decode("utf-8")))
-
