837adaf517676ae16b92c86ed7f32fbe773462b8
terry677 <terryabu67@gmail.com>
2023-04-02 14:07:13 -0700

    Added task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Write a Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+if __name__ == '__main__':
+    import requests
+    response = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
